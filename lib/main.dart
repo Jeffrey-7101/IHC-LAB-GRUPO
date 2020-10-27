5d37fae5649f76d0e595dab887787677e5f523e8
CesarVasquez2001 <cvasqueza@unsa.edu.pe>
2020-10-26 20:31:09 -0500

    delete a tab

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:pruebas_en_flutter/tabs/ayuda_tabs.dart';
 import 'package:pruebas_en_flutter/tabs/inicio_tabs.dart';
-import 'package:pruebas_en_flutter/tabs/lugares_tabs.dart';
 import 'package:pruebas_en_flutter/tabs/registro_tabs.dart';
-
+import 'package:flutter/foundation.dart';
 
 void main() {
+  var movieTitle = "hola";
   runApp(MyApp());
+  print('$movieTitle');
 }
+
 class MyApp extends StatelessWidget {
   Widget build(context) {
     return MaterialApp(
@@ -33,29 +35,28 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         primarySwatch: Colors.blue,
       ),
       home: DefaultTabController(
-        length: 3, 
-        child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text("Estudio Numerologico"),
-            bottom: new TabBar(
-              indicatorColor: Colors.white,
-              tabs: <Widget>[
-                new Tab(icon: new Icon(Icons.home), text: "Inicio"),
-                new Tab(icon: new Icon(Icons.contacts), text: "Ingresar"),
-                new Tab(icon: new Icon(Icons.help), text: "Ayuda")
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text("Estudio Numerologico"),
+              bottom: new TabBar(
+                indicatorColor: Colors.white,
+                tabs: <Widget>[
+                  new Tab(icon: new Icon(Icons.home), text: "Inicio"),
+                  new Tab(icon: new Icon(Icons.contacts), text: "Ingresar"),
+                  new Tab(icon: new Icon(Icons.help), text: "Ayuda")
+                ],
+              ),
+            ),
+            body: new TabBarView(
+              children: <Widget>[
+                new InicioTabs(),
+                new RegistroTabs(),
+                new AyudaTabs(),
               ],
             ),
-          ),
-          body: new TabBarView(
-            children: <Widget>[
-              new InicioTabs(),
-              new RegistroTabs(),
-              new AyudaTabs(),
-            ],
-          ),
-        )
-      ),
+          )),
     );
   }
 }
